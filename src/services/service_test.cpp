8f989c5e24303f2248771e999219f98a9ac84b57
squidpie <squidpie_meowface@pm.me>
2022-12-28 15:20:51 -0800

    swapped service_test to mock_event_server

--- src/services/service_test.cpp
+++ src/services/service_test.cpp
@@ -1,20 +1,40 @@
+#include "gmock/gmock.h"
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "events.h"
 #include "service.h"
 
 const std::string TEST_LIB_PATH = "/home/squidpie/Repos/squidbot/targets/lib/test";
 
+class MockEventClient : public EventClientBase {
+public:
+  MOCK_METHOD(void, subscribe, (EventType));
+  MOCK_METHOD(void, send, (Event));
+  MOCK_METHOD(const Event, receive, ());
+};
+
+class MockEventServer : public EventServerBase {
+public:
+  MOCK_METHOD(EventClientBase*, create_client, ());
+};
+
 class ServiceTest: public testing::Test {
   protected:
     ServiceContainer_t * container_dut;
     ServiceContainer_t * error_dut;
-    EventServer mock_event_server;
+
+    MockEventServer mock_event_server;
+    MockEventClient mock_event_client;
 
     void SetUp() {
       plog::init(plog::debug, "mock.log");
       Context_t context = {plog::get(), &mock_event_server};
 
+      ON_CALL(mock_event_server, create_client())
+          .WillByDefault(testing::Return(&mock_event_client));
+
+      EXPECT_CALL(mock_event_server, create_client);
       container_dut = new ServiceContainer("mockservice", &context);
       error_dut = new ServiceContainer("no_lib_found_error", &context);
 
