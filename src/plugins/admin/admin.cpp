ced5c08b3478583975016b7f005a0e6de26323d0
squidpie <squidpie_meowface@pm.me>
2023-01-14 11:30:40 -0800

    Adds framework for Admin plugin

---
+++ src/plugins/admin/admin.cpp
@@ -0,0 +1,23 @@
+#include "admin.h"
+
+AdminAction::AdminAction(InterfaceMap_t *_interfaces,
+                         std::shared_ptr<PluginDataBase>)
+    : interfaces(_interfaces) {
+  mockservice = std::dynamic_pointer_cast<MockServicePluginInterface>(
+      interfaces->at(std::type_index(typeid(MockService))));
+}
+
+bool AdminFilter::is_trigger(Event rx) { return (rx.type == TEST_EVENT_TYPE); }
+
+void AdminAction::trigger_action(Event rx) { mockservice->test(); }
+
+/*
+create_interfaces : load the interface map with all interfaces the
+trigger_action will require to interact with
+ */
+InterfaceMap_t Admin::create_interfaces(std::shared_ptr<CoreContext> context) {
+  InterfaceMap_t interfaces;
+  interfaces[std::type_index(typeid(MockService))] =
+      context->service_manager->get_interface<MockService>();
+  return interfaces;
+}
--- src/squidbot.cpp
+++ src/squidbot.cpp
@@ -1,58 +1,12 @@
 #include "squidbot.h"
 
-class TestData : virtual public ServiceDataBase {
-  ~TestData() {}
-};
-
-class TestRunActionContext : virtual public RunActionContextBase {
-public:
-  TestRunActionContext(std::shared_ptr<EventClientBase> event_client,
-                       std::shared_ptr<TestData> data)
-      : event_client(event_client), data(data) {}
-  ~TestRunActionContext() {}
-  std::shared_ptr<EventClientBase> event_client;
-  std::shared_ptr<TestData> data;
-};
-
-class TestRunAction : virtual public RunActionBase {
-public:
-  typedef TestRunActionContext context_t;
-  TestRunAction(std::shared_ptr<TestRunActionContext> _context) {}
-  ~TestRunAction() {}
-  void run_action() {
-    PLOGD << "MEOW";
-    sleep(5);
-  }
-};
-
-class TestPluginInterface : virtual public ServiceInterfaceBase {
-public:
-  TestPluginInterface(std::shared_ptr<TestData> data) : data(data) {}
-  ~TestPluginInterface() {}
-  protected:
-    std::shared_ptr<TestData> data;
-};
-
-class TestExternalInterface : virtual public ServiceInterfaceBase {
-public:
-  ~TestExternalInterface() {}
-};
-
-class TestService {
-public:
-  typedef TestRunAction run_action_t;
-  typedef TestPluginInterface plugin_interface_t;
-  typedef TestExternalInterface external_interface_t;
-  typedef TestData data_t;
-};
-
 int main(int argc, char **argv) {
   plog::init(plog::debug, "squidbot.log");
   auto event_server = std::make_shared<EventServer>();
   auto  plugin_manager = std::make_shared<PluginManager>();
   auto service_manager = std::make_shared<ServiceManager>();
 
-  std::shared_ptr<CoreContext> context = std::make_shared<CoreContext>(plog::get(), event_server, service_manager, plugin_manager, TEST_LIB_DIR);
+  std::shared_ptr<CoreContext> context = std::make_shared<CoreContext>(plog::get(), event_server, service_manager, plugin_manager, LIB_DIR);
   service_manager->load(context);
   plugin_manager->load(context);
   plugin_manager->unload();
